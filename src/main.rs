6449c89a0b0025ada1f4daddce8ea3c90abcce80
johncartergonzalez <unmrf0ngh@mozmail.com>
2023-10-17 16:59:27 -0700

    feat: impl stdin support

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,19 @@ use clap::{App, Arg};
 use regex::Regex;
 use std::{
     fs::File,
+    intrinsics::atomic_singlethreadfence_acqrel,
     io::{prelude::*, BufReader},
 };
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => print!("{}", line),
+            None => {}
+        }
+    }
+}
 
 fn main() {
     let args = App::new("grep-lite")
@@ -28,14 +39,13 @@ fn main() {
     let re = Regex::new(needle).unwrap();
 
     let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
-
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            Some(_) => print!("{}", line),
-            None => {}
-        }
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
